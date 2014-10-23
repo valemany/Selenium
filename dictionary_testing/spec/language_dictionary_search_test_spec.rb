da17421e9f34d4f258c11de76686e0432c89573f
Victor <valemany@shutterstock.com>
2014-10-22 20:44:54 -0400

    Adding spec helper, more refactoring

--- dictionary_testing/spec/language_dictionary_search_test_spec.rb
+++ dictionary_testing/spec/language_dictionary_search_test_spec.rb
@@ -1,24 +1,20 @@
 #!/usr/bin/env ruby
 # This script grabs search terms from a csv/excel file and tests if there are search results for those. 
 
-require 'csv'
-require 'rspec'
-require 'pry'
-require 'colorize'
+require_relative 'spec_helper'
 
-RSpec.configure { |c| c.run_all_when_everything_filtered = true }
+# RSpec.configure { |c| c.run_all_when_everything_filtered = true }
 
 Dir["./lib/*"].each {|file| require file }
 
-describe 'Dictionary search test based on country code' do
-	before :all do
-		@LANGUAGE = ENV['LANGUAGE'].downcase.gsub('@','').to_s
-		puts "Running test in #{Browser::SITE} using #{@LANGUAGE} language".colorize(:blue)
-		@browser = Browser.new 
-		@search = Search.new @browser
+language ||= ENV['LANGUAGE'].downcase.gsub('@','').to_s
+	if language == 'all' then
+		language = ["cs", "da", "de", "fi", "hu", "it", "ja", "nb", "nl", "pl", "ru", "sv", "th", "tr", "zh"] 
 	end
 
+describe 'Dictionary search test based on country code' do
 	before :each do
+		@search = Search.new @browser
 		@total_pass = 0
 		@total_fail = 0 
 		@total_searches = 0 
@@ -27,29 +23,49 @@ describe 'Dictionary search test based on country code' do
 	after :each do
 		puts "##{@total_searches} total searches were performed!".colorize(:green)
 		puts "##{@total_fail} searches returned no results".colorize(:red)
-		puts "##{@total_pass} search terms returned results using #{@LANGUAGE} language from (#{Browser::SITE})".colorize(:green)
 	end
 
-	after :all do
-		@browser.quit
-	end
-
-	it "Verifies search results for #{@LANGUAGE} language", "#{@LANGUAGE}".to_sym do 
-		CSV.foreach(File.expand_path("test_data_#{@LANGUAGE}.csv"),encoding: "UTF-8", headers:true, col_sep:",") do |row|
-			begin
+	if language.class != Array
+		it "Verifies search results for #{language} language", "#{language}".to_sym do 
+			puts "Running test in #{Browser::SITE.upcase} using #{language} language".colorize(:blue)
+			CSV.foreach(File.expand_path("test_data_#{language}.csv"),encoding: "UTF-8", headers:true, col_sep:",") do |row|
+				begin
 				@total_searches += 1
 				search_term = CGI.escape(row[0].to_s)
-				@browser.visit (@LANGUAGE + "/cat.mhtml?searchterm=#{search_term}")
+				@browser.visit (language + "/cat.mhtml?searchterm=#{search_term}")
 				if expect(@search.grid_cell).to be 
 					puts "Search term #{row[0]} - PASSED" 
 					@total_pass += 1
 				end 
-			rescue Exception
-				@total_fail += 1 
-				puts "Did not return search results for #{row[0]}".colorize(:yellow)
+				rescue Exception
+					@total_fail += 1 
+					puts "Did not return search results for #{row[0]}".colorize(:yellow)
+				end
 			end
   		end
   	end
+
+	if language.class == Array
+		language.each do |lang|
+			it "Verifies search results for all languages", :all do 
+				puts lang.upcase
+				CSV.foreach(File.expand_path("test_data_#{lang}.csv"),encoding: "UTF-8", headers:true, col_sep:",") do |row|
+					begin
+						@total_searches += 1
+						search_term = CGI.escape(row[0].to_s)
+						@browser.visit (lang + "/cat.mhtml?searchterm=#{search_term}")
+						if expect(@search.grid_cell).to be 
+							puts "Search term #{row[0]} - PASSED" 
+							@total_pass += 1
+						end 
+					rescue Exception
+						@total_fail += 1 
+						puts "Did not return search results for #{row[0]}".colorize(:yellow)
+					end
+				end
+			end
+		end
+	end
 
   # 	it 'Verifies search results for da language', :da do 
 		# CSV.foreach(File.expand_path("test_data_da.csv"),encoding: "UTF-8", headers:true, col_sep:",") do |row|
